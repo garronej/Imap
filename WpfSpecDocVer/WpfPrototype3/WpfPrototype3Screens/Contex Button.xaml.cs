77ebe074f4478a5fbc7843ff7c5c7d6b2fb55c16
Garrone Joseph <joseph.garrone@ensimag.grenoble-inp.fr>
2014-05-22 01:03:11 +0200

    Initialisation

---
+++ WpfSpecDocVer/WpfPrototype3/WpfPrototype3Screens/Contex Button.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPrototype3Screens
+{
+	/// <summary>
+	/// Interaction logic for Contex_Button.xaml
+	/// </summary>
+	
+	public partial class Contex_Button : UserControl
+	{
+		public Contex_Button()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
---
+++ WpfSpecDocVer/WpfPrototype3/WpfPrototype3Screens/CustomSearch.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace WpfPrototype3Screens
+{
+	/// <summary>
+	/// Interaction logic for CustomSearch.xaml
+	/// </summary>
+	public partial class CustomSearch : UserControl
+	{
+		
+		public CustomSearch()
+		{
+			this.InitializeComponent();
+			
+			// Required to initialize variables
+			InitializeComponent();
+           
+		}
+	}
+}
