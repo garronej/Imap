2941e5470eb5bf5466e3cec60a0e8f6eb4d8f03a
Garrone Joseph <joseph.garrone@ensimag.grenoble-inp.fr>
2014-05-23 00:55:14 +0200

    Enin

---
+++ Prototype/Imap/ImapScreens/SearchComponentUI.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace ImapScreens
+{
+	public partial class SearchComponentUI : UserControl
+	{
+		List<String> Items = new List<String>();
+		public SearchComponentUI()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+			
+			Items.Add("Ensimag");
+            Items.Add("Phelma");
+			
+			
+			 // Set the data context for the AutoCompleteBox.
+            myACB.DataContext = Items;
+		}
+	}
+}
