f9ac98e902f6fd61d8efeb2c2a172fd14e3d3fc5
Garrone Joseph <joseph.garrone@ensimag.grenoble-inp.fr>
2014-05-22 05:14:37 +0200

    voila

--- Prototype/Imap/ImapScreens/Search component.xaml.cs
+++ Prototype/Imap/ImapScreens/Search component.xaml.cs
@@ -9,27 +9,22 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Collections.Generic;
 
+
 namespace ImapScreens
 {
 	public partial class Search_component : UserControl
 	{
+		
 		List<String> Items = new List<String>();
 		public Search_component()
 		{
-			// Required to initialize variables
-			InitializeComponent();
 			
 			// Required to initialize variables
-            Items.Add("One");
-            Items.Add("Two");
-            Items.Add("Three");
-            Items.Add("Four");
-            Items.Add("Five");
-            Items.Add("Six");
-            Items.Add("Seven");
-            Items.Add("Eight");
-            Items.Add("Nine");
-            Items.Add("Ten");
+			InitializeComponent();
+
+            Items.Add("Ensimag");
+            Items.Add("Phelma");
+			
 			
 			 // Set the data context for the AutoCompleteBox.
             myACB.DataContext = Items;
